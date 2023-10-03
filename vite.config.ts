a8824be22a824a7ea13729c68a8b3623f3a5a2e3
Cycle9898 <cycle9898@gmail.com>
2023-10-03 12:01:31 +0200

    Initial commit: files and folders organization + deleted useless files + added and full set up ViteJS' Library Mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ['lib'] })
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname,'lib/main.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react','react/jsx-runtime',"react-dom"],
+    }
+  }
 })
